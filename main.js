f87456af15d995a937eabcfe07b6e987d10e431b
ProtacioUno <enervinluan01@gmail.com>
2024-03-12 15:44:04 +0800

    015 hehe

--- main.js
+++ main.js
@@ -1,112 +1,68 @@
 
 // elemenks = document.querySelector('.skill_container') ; 
 elemenks = document.querySelector('#content') ; 
-// proj_showcase = document.querySelector('.project_showcase'); 
+// let proj_showcase = document.querySelector('.project_showcase'); 
 
 // elemenks.addEventListener("click", myFunction);
 
 
 
-elemenks.addEventListener("click", myFunction);
-
-// function myFunction(){
-//   alert('cleck')
-// }
-
-
+// proj_showcase.addEventListener("click", myFunction);
 
 // INITIALIZE NEEDS 
-function myFunction(e) {
-    // console.log
-    console.log(e)
-    proj_showcase.innerHTML = "Project <br> Showcase " ;  
+// function myFunction(e) {
+  // console.log
+    // console.log(e)
+    // proj_showcase.innerHTML = "Project <br> Showcase " ;  
     // proj_inner = proj_showcase.innerHTML ; 
     // proj_inner = "nice" ; 
     // print
     // alert(proj_inner);   
-
+    
     // alert(typeof proj_inner);   
     // cnosole.log(proj_inner); 
     // alert(proj_showcase.innerHTML ) ;
     // alert("Hello World!");
-}
-
-
-// window.addEventListener('load' , (e)=>{
-//     startQueries();
-//     // console.log(e) ; 
-// })
-
-
-
-// const startQueries = () => {
-//     const media_query = window.matchMedia('( min-width : 400px )')
-    
-//     console.log(e)
-    
-//     if(media_query.matches){
-//         console.log("it matehces !  ")
-//     //    alert('matches!') ; 
-//         // console.log(media_query)
-//     }
-//     // print(media_query.addEventListener)
-
-
-    
-// }
- 
-// window.addEventListener('load' , () => {
-//     startQueries(); 
-// })
-
-
-// // Create a MediaQueryList object for max-width: 700px
-// const mediaQuery = window.matchMedia("(min-width: 400px)");
-
-// // Define a function to handle changes
-// function handleViewportChange(event) {
-//     if (event.matches) {
-//         // If viewport is <= 700px wide
-//         document.proj_showcase.style.backgroundColor = "yellow";
-//     } else {
-//         // If viewport is > 700px wide
-//         document.proj_showcase.backgroundColor = "pink";
-//     }
-// }
-
-// // Call the function initially
-// handleViewportChange(mediaQuery);
-
-// // Attach listener for changes
-// mediaQuery.addEventListener("change", handleViewportChange);
-
-
-// const tool_lest = document.querySelectorAll('.tool_list');
-
-
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      
-      console.log("singkamas ", tool_lest.innerHTML)
-      // alert()
-        // alert('higher !'); 
-    //   document.bodasdasy.style.backgroundColor = "yellow";
+  // }
+  
+  
+  
+  
+  //   Create a MediaQueryList object
+  let x = window.matchMedia("(max-width: 500px)")
+  let proj_showcase = document.querySelector('.project_showcase'); 
+  // let mediaQuery = window.matchMedia('(max-width:500px') ;    
+  
+  window.addEventListener('resize',()=>{
+    // console.log('this changess ! '); 
+    if(window.innerWidth < 500){
+      console.log('it is less than');           
+      proj_showcase.innerHTML = "Project Showcase " ;  
     } else {
-        alert('lower !')
-    //   document.body.style.backgroundColor = "pink";
-    }
+      console.log('it is greater than') ; 
+      proj_showcase.innerHTML = "Project <br> Showcase " ;  
   }
 
-//   Create a MediaQueryList object
-  var x = window.matchMedia("(max-width: 500px)")
-   
+  console.log('why it ino working ? '); 
+  
+  // if(mediaQuery.matches){
+  //   console.log('it is less than');     
+  // } else {
+  //   console.log('it is greater than') ; 
+  // }
+})
+
+
+
+
   // Call listener function at run time
   // myFunction(x);
   
   // Attach listener function on state changes
-  x.addEventListener("change", function() {
-    myFunction(x);
-  }); 
+  // x.addEventListener("change", function(){
+    
+  //     // myFunction();
+  // }); 
 
 
 
